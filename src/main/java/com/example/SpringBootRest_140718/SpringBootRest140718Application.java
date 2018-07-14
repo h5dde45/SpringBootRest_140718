eb2e26095cc29f17115cbe43eea292d1cc5b8bf0
tmvf <tmvf@yandex.ru>
2018-07-14 14:40:39 +0300

    01

---
+++ src/main/java/com/example/SpringBootRest_140718/SpringBootRest140718Application.java
@@ -0,0 +1,12 @@
+package com.example.SpringBootRest_140718;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRest140718Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRest140718Application.class, args);
+	}
+}
